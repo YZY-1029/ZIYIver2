880d414e6682f118b282f50561f1880e2234a19c
YZY-1029 <jasom.102935@yahoo.com.tw>
2025-05-20 15:23:47 +0800

    0520_0323

--- JavaWeb/src/main/java/servlet/WelcomeServlet.java
+++ JavaWeb/src/main/java/servlet/WelcomeServlet.java
@@ -2,52 +2,37 @@ package servlet;
 
 import java.io.IOException;
 
+import Utul.util;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/welcome")  //給子分業設定子網址
+
+@WebServlet("/welcome")
 public class WelcomeServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//設定編碼
-		resp.setCharacterEncoding("UTF-8");  //告訴Servlet我的編碼方式是 UTF-8
-		resp.setContentType("text/html;charset=UTF-8");  //告訴瀏覽器我裡面的編碼方式是 UTF-8(萬國碼
+		// 設定編碼
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html;charset=UTF-8");
 		
-		//接收參數
-		//例如: http://localhost:8080/JavaWeb/welcome?name=Jack&age=17
-		String name = req.getParameter("name");  //因為要讓參數在網址內,所以這段參數他是req
+		// 接收參數
+		// 例如: http://localhost:8080/JavaWeb/welcome?name=Jack&age=17
+		String name = req.getParameter("name");
 		String age = req.getParameter("age");
 		
-		//檢查age是否為int
-		if(isNumber(age)) {
-			String message = Integer.parseInt(age) >= 18 ?"成年":"未成年";
-			resp.getWriter().print(name+"歡迎光臨(" + age + "歲" + message +")");
-		}else
-			resp.getWriter().print(name+"歡迎光鈴");
-		
-		//判斷是否是數字
-		private boolean isNumber(String data) {
-			try {
-				Integer.paraInt(data);
-				return true;
-			} catch (Exception e) {
-				return false;
-			}
-		}
-		//使用正則表示式
-		private boolean isNumber2(String age) {
-			
-			
+		// 檢查 age 是否是數字
+		if(util.isNumber2(age)) {
+			String message = Integer.parseInt(age) >= 18 ? "成年" : "未成年";
+			resp.getWriter().print(name + " 歡迎光臨 (" + age + " 歲 " + message +")");
+		} else {
+			resp.getWriter().print(name + " 歡迎光臨");
 		}
 		
-		
-		
-		
-		//String message = Integer.parseInt(age) >=18?"成年":"未成年";
-		//resp.getWriter().println(name+"歡迎光臨(" + age + "歲)");
 	}
+	
+	
 }
